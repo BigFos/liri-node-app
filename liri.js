4191b0e19066fd457629deada49feecbdc564d21
Steven Foster Jr <Steven Foster Jr>
2017-06-24 01:05:56 -0700

    twitter and spotify working

--- liri.js
+++ liri.js
@@ -18,33 +18,27 @@ if (pro[2] === "my-tweets") {
             console.log(error);
         }
     });
-}
+} else if (pro[2] === "spotify-this-song") {
 
-else if(pro[2] === "spotify-this-song"){
+    var track = "";
 
- 
-var spotify = new Spotify(keys.spotifyKeys);
-// console.log(keys.spotifyKeys);
-spotify
-  .search({ type: 'track', query: 'all the small things' })
-  .then(function(response) {
-    for (var j = 0; j < response.tracks.items.length; j++) {
+    for (var n = 3; n < pro.length; n++) {
+        track = track + " " + pro[n];
+    }
+
+    var spotify = new Spotify(keys.spotifyKeys);
+
+    spotify
+        .search({ type: 'track', query: track })
+        .then(function(response) {
+            for (var j = 0; j < response.tracks.items.length; j++) {
                 console.log("\nArtist: " + response.tracks.items[j].artists[0].name);
                 console.log("Name: " + response.tracks.items[j].name);
                 console.log("URL: " + response.tracks.items[j].external_urls.spotify);
                 console.log("Album: " + response.tracks.items[j].album.name);
-    
-}
-    // console.log(response);         
-  })
-  .catch(function(err) {
-    console.log(error);
-  });
-// spotify.search({ type: 'track', query: 'I will always love you' }, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
- 
-// console.log(data); 
-// });
+            }
+        })
+        .catch(function(err) {
+            console.log(error);
+        });
 }
