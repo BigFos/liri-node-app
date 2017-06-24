febd2d43239ebb5b6e4d346b6e8f13d9bbe976b3
Steven Foster Jr <Steven Foster Jr>
2017-06-24 12:02:56 -0700

    prettified code

--- liri.js
+++ liri.js
@@ -13,7 +13,7 @@ if (pro[2] === "my-tweets") {
     client.get('statuses/user_timeline', params, function(error, tweets) {
         if (!error) {
             for (var i = 0; i < tweets.length; i++) {
-                console.log("\n"+tweets[i].created_at);
+                console.log("\n" + tweets[i].created_at);
                 console.log(tweets[i].text);
             }
         } else {
@@ -23,14 +23,13 @@ if (pro[2] === "my-tweets") {
 } else if (pro[2] === "spotify-this-song") {
 
     var track = "";
-    if(pro[3] == null){
-    	track = "The Sign"
+    if (pro[3] == null) {
+        track = "The Sign"
+    } else {
+        for (var n = 3; n < pro.length; n++) {
+            track = track + " " + pro[n];
+        }
     }
-    else{
-    for (var n = 3; n < pro.length; n++) {
-        track = track + " " + pro[n];
-    }
-	}
     var spotify = new Spotify(keys.spotifyKeys);
 
     spotify
@@ -46,46 +45,42 @@ if (pro[2] === "my-tweets") {
         .catch(function(err) {
             console.log(error);
         });
-}
-else if(pro[2] === "movie-this"){
-	 var title = "";
-    if(pro[3] == null){
-    	title = "Mr+Nobody"
+} else if (pro[2] === "movie-this") {
+    var title = "";
+    if (pro[3] == null) {
+        title = "Mr+Nobody"
+    } else {
+        for (var n = 3; n < pro.length; n++) {
+            title = title + "+" + pro[n];
+        }
     }
-    else{
-    for (var n = 3; n < pro.length; n++) {
-        title = title + "+" + pro[n];
-    }
-	}
-request("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
-  // If the request is successful (i.e. if the response status code is 200)
-  if (!error && response.statusCode === 200) {
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log("\nTitle: " + JSON.parse(body).Title);
-    console.log("Year: " + JSON.parse(body).Year);
-    console.log("Rating: " + JSON.parse(body).imdbRating);
-    console.log("Country: " + JSON.parse(body).Country);
-    console.log("Language: " + JSON.parse(body).Language);
-    console.log("Plot: " + JSON.parse(body).Plot);
-    console.log("Actors: " + JSON.parse(body).Actors);
-    console.log("Website: " + JSON.parse(body).Website);
-  }
-});
-}
-else if(pro[2] === "do-what-it-says"){
-fs.readFile("random.txt", "utf8", function(error, data) {
-  // If the code experiences any errors it will log the error to the console.
-  if (error) {
-    return console.log(error);
-  }
-  // We will then print the contents of data
-  // console.log(data);
-  // Then split it by commas (to make it more readable)
-  var dataArr = data.split(",");
-  // We will then re-display the content as an array for later use.
-  // console.log(dataArr);
-  pro[2] = dataArr[0];
-  pro[3] = dataArr[1];
-});
+    request("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+
+            console.log("\nTitle: " + JSON.parse(body).Title);
+            console.log("Year: " + JSON.parse(body).Year);
+            console.log("Rating: " + JSON.parse(body).imdbRating);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("Website: " + JSON.parse(body).Website);
+        }
+    });
+} else if (pro[2] === "do-what-it-says") {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+
+        if (error) {
+            return console.log(error);
+        }
+
+        // console.log(data);
+
+        var dataArr = data.split(",");
+
+        // console.log(dataArr);
+        pro[2] = dataArr[0];
+        pro[3] = dataArr[1];
+    });
 }
